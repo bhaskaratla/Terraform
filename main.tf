023167be14720c866ef7a807de3545693af65396
bhaskaratla <bhaskaratla@gmail.com>
2022-10-31 13:10:09 +0530

    credentials added

--- main.tf
+++ main.tf
@@ -1,13 +1,7 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
 provider "aws" {
   region = "ap-south-1"
+  shared_credentials_file = "/.aws/credentials"
+  profile = "testing"
 }
 
 resource "aws_vpc" "abkr-vpc" {
