575e93fcabc5376491e09e0b4352deaab8ea2121
bhaskaratla <bhaskaratla@gmail.com>
2022-10-31 18:54:55 +0530

    removed credentials in main file

--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
 provider "aws" {
   region = "eu-west-2"
-  aws_access_key_id = AKIATF76WA6SMQVZPZ4Q
-  aws_secret_access_key = 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
 }
 
 resource "aws_vpc" "abkrvpc" {
